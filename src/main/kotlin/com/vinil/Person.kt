c70e807b8f90aa56c50b66e049a5ac35e0bfbc55
viniljose <vinilpj@gmail.com>
2023-02-20 11:55:54 +0530

    Null Safty

--- src/main/kotlin/com/vinil/Person.kt
+++ src/main/kotlin/com/vinil/Person.kt
@@ -3,7 +3,7 @@ package com.vinil
 import java.time.LocalDate
 import java.time.Month
 
-data class Person(val first: String, val last:String, val dob:LocalDate )
+data class Person(val first: String?=null, val last:String, val dob:LocalDate )
 
 fun main(){
     val name1 = Person("Rocky","Rocks",LocalDate.of(1988,Month.APRIL,10))
@@ -28,4 +28,14 @@ fun main(){
     println(map)
     for((key,value) in map)
         println("$key maps to $value")
+
+    //Null Safety
+    val brian = Person(last="Lara",dob=LocalDate.of(1982,Month.APRIL,12))
+    val(f1,l1,_)=brian
+    if(f1 != null)
+        println("$f1 $l1")
+    else
+        println(l1)
+    println("length of first name is ${f1?.length}")
+    println("length of first name is ${f1!!.length}") //This will result in Null Pointer Exception
 }
