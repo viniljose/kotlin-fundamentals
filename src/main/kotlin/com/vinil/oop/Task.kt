7afc8e3d663de2df766a65d23705fa1290c0a25a
viniljose <vinilpj@gmail.com>
2023-02-21 08:45:52 +0530

    class sample

--- src/main/kotlin/com/vinil/oop/Task.kt
+++ src/main/kotlin/com/vinil/oop/Task.kt
@@ -1,8 +1,19 @@
 package com.vinil.oop
 
-class Task(val name:String) {
-    var property=3;
+class Task(val name:String,_priority:Int=DEFAULT_PRIORITY) {
+    var priority=validPriority(_priority);
+   // set(value){
+     //   priority = validPriority(value)
+    //}
+
+companion object{
+    const val MIN_PRIORITY = 1
+    const val MAX_PRIORITY = 5
+    const val DEFAULT_PRIORITY = 3
+}
+    private fun validPriority(p:Int)=p.coerceIn(MIN_PRIORITY, MAX_PRIORITY)
 }
 fun main(){
-   // var myTask = Task().apply { property=4 }
+    var myTask = Task("First", 9)
+    println(myTask.priority)
 }
